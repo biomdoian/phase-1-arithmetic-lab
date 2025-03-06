7c961013c55ef44ea44699b152a7db30005a5a51
Ian <iankruze60@gmail.com>
2025-03-06 15:07:46 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+
+function other(a,b) {
+ 
+  return num1*num2;
+}
+
+let multiply = other(num1,num2);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let max = Math.max(1, 5, 10, 20, 3);
+
+let result = mod(num3, num4);
+console.log(result); 
